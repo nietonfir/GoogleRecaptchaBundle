4a63abfa3f0d8f631bb575e9be123dfda3410a51
nietonfir <nietonfir@gmail.com>
2015-01-13 00:49:58 +0100

    Refactor ReCaptchaResponseValidator to use the exposed revisor service
    for proper validating the user's response.

--- Validator/Constraints/ReCaptchaResponseValidator.php
+++ Validator/Constraints/ReCaptchaResponseValidator.php
@@ -7,28 +7,27 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class ReCaptchaResponseValidator extends ConstraintValidator
 {
-    private $reCaptcha;
+    private $revisor;
 
-    public function __construct($reCaptcha)
+    public function __construct($revisor)
     {
-        $this->reCaptcha = $reCaptcha;
+        $this->revisor = $revisor;
     }
 
     public function validate($value, Constraint $constraint)
     {
-        /*
-        // FIXME - noop
-        if (!preg_match('/^[a-zA-Za0-9]+$/', $value, $matches)) {
+        $response = $this->revisor->verify($value);
+        if (!$response->isValid()) {
+            var_dump($response->getErrors());
+
             // If you're using the new 2.5 validation API (you probably are!)
-            $this->context->buildViolation($constraint->message)
-                ->addViolation();
+            $this->context->buildViolation($constraint->message)->addViolation();
 
             // If you're using the old 2.4 validation API
-            $this->context->addViolation(
-                $constraint->message,
-                array('%string%' => $value)
-            );
+            // $this->context->addViolation(
+            //     $constraint->message,
+            //     array('%string%' => $value)
+            // );
         }
-        */
     }
 }
